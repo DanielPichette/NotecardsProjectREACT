d89db51ec92660633982edf36d6dc1f8c0480dec
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-07 11:16:28 -0500

    updated navigation bar. added possible buttons

--- src/Components/NavigationBar/navaigationBar.jsx
+++ src/Components/NavigationBar/navaigationBar.jsx
@@ -8,7 +8,11 @@ function Navigation(props){
 
 return (
         <div class='navbody'>
-            <h4>Navigation</h4>
+            <h4>Collections</h4>
+            <h4>Quiz Me!</h4>
+            <h4>All Cards</h4>
+            <h4>New Collection</h4>
+            <h4>New Card</h4>
         </div>
     );
 
