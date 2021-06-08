c18bc1d11d5ce425d3440fa7c54402162ec86bef
DanielPichette <daniel.f.pichette@gmail.com>
2021-06-08 11:10:06 -0500

    added logic to the quiz.jsx file.

--- src/Components/Quiz/quiz.jsx
+++ src/Components/Quiz/quiz.jsx
@@ -9,16 +9,45 @@ class Quiz extends Component {
             allCollections: [],
             allCards: [],
             currentCollection: 0,
-            currentDeck: [],
+            deck: [],
+            cardIndex: 0,
         }
     }
-}
-render(){
+    nextCard(){
+        let tempCard = this.state.cardIndex;
+        tempCard ++
+        if(tempCard === this.deck.length){
+            tempCard = 0;
+        }
+        this.setState({
+            cardIndex: tempCard
+        });
+    }
+
+    previouseCard(){
+        let tempCard = this.state.cardIndex;
+        tempCard --;
+        if(tempCard < 0){
+            tempCard = this.deck.length - 1;
+        }
+        this.setState({
+            cardIndex: tempCard
+        });
+    }
+
+    render(){
     return(
-        <div>
-            
+        <div QuizBaody>
+            <button> Previos card</button>
+            <div>
+                <h1>{this.deck[this.cardIndex].word}</h1>
+                <h4>{this.deck[this.cardIndex].definition}</h4>
+            </div>
+            <button onClick= {() => this.nextCard()} >Next card</button>
+
         </div>
-    )
+        )
+    }
 }
      
     export default Quiz;
